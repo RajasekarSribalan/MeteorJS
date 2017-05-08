ff7cd622b841ddbc0c9f70f9124fab29fbd75717
Rajasekar <emailtoraj90@gmail.com>
2017-05-08 07:24:12 +0530

    Included Session object for reactive

--- tasklist/client/main.js
+++ tasklist/client/main.js
@@ -1,9 +1,25 @@
+import { Session } from 'meteor/session'
+Session.set("todos", [
+  { label: "Buy milk", done: true },
+  { label: "Upload photos" },
+  { label: "Dess up quickly" }
+]);
+
+
 Template.todoList.helpers({
   todos: function () {
-    return [
-      { label: "Buy milk", done: true }, 
-      { label: "Upload photos" }, 
-      { label: "Dess up quickly" }
-    ]
+    return Session.get("todos");
+  }
+});
+
+Template.todoList.events({
+  "click .add-todo": function () {
+    console.log("added todos...");
+    var todos = Session.get("todos");
+    todos.push({
+      label: "New todo"
+    });
+
+    Session.set("todos", todos)
   }
 });
