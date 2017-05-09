89fda956e860c64dea1a09121a40e6eb6d5e4c7e
Rajasekar <emailtoraj90@gmail.com>
2017-05-09 07:12:22 +0530

    Added todo checked and removal logic

--- tasklist/client/main.js
+++ tasklist/client/main.js
@@ -17,3 +17,21 @@ Template.todoList.events({
     })
   }
 });
+
+Template.todo.events({
+  "click input" : function(){
+    var isDone = Todos.findOne({_id: this._id}).done;
+    Todos.update({_id: this._id},{$set : {done : !isDone}});
+  },
+
+  "click .delete": function (){
+    Todos.remove({_id: this._id});
+  }
+});
+
+
+Template.todo.helpers({
+  done: function () {
+    return Todos.findOne({_id: this._id}).done;
+  }
+});
